5900319bb414aa4ce4721c0fca99d2543901aedf
bubble-tok <mikhaylajung@gmail.com>
2026-07-13 15:50:44 -0400

    add permissions

--- time_announcement/lib/services/permission_service.dart
+++ time_announcement/lib/services/permission_service.dart
@@ -1,1 +1,16 @@
+import 'package:permission_handler/permission_handler.dart';
 
+class PermissionService {
+  Future<PermissionStatus> notificationStatus() =>
+      Permission.notification.status;
+
+  Future<PermissionStatus> exactAlarmStatus() =>
+      Permission.scheduleExactAlarm.status;
+
+  Future<void> requestAllPermissions() async {
+    await [Permission.notification, Permission.scheduleExactAlarm].request();
+  }
+
+  // Opens the device's app settings page, for when a permission is denied
+  Future<bool> openSettings() => openAppSettings();
+}
